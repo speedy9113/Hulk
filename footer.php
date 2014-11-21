013da3b3d7fd9f5b53369e5b2baa09b68a331d5a
Ingo Malchow <ingomalchow@gmail.com>
2014-11-21 20:18:54 +0100

    reformat footer, blue covers full screen

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
 </section>
-<footer class="row">
-	<?php do_action('foundationPress_before_footer'); ?>
-	<?php dynamic_sidebar("footer-widgets"); ?>
-	<?php do_action('foundationPress_after_footer'); ?>
+<footer>
+    <div class="row">
+        <?php do_action('foundationPress_before_footer'); ?>
+        <?php dynamic_sidebar("footer-widgets"); ?>
+        <?php do_action('foundationPress_after_footer'); ?>
+    </div>
 </footer>
 <a class="exit-off-canvas"></a>
 
